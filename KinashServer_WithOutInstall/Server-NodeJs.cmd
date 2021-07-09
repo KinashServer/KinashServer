136d6b280a9ff801d040932d17a40f2f79e10366
andriy332 <f147034@ukr.net>
2021-07-09 11:56:01 +0300

    Server

---
+++ KinashServer_WithOutInstall/Server-NodeJs.cmd
@@ -0,0 +1,5 @@
+@echo off
+@echo node C:\Users\%username%\Documents\KinashServer\app1.js > %temp%/temp.bat
+@echo node C:\Users\%username%\Documents\KinashServer\http\443Fix.js > %temp%/temp2.bat
+start %temp%/temp2.bat
+start %temp%/temp.bat
