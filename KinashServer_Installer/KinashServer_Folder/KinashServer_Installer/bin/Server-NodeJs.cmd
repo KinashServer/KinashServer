7026209cc7d81f55508c688ee33daa24d20b6d18
andriy332 <75567134+andriy332@users.noreply.github.com>
2021-07-14 12:23:38 +0300

    Update Server-NodeJs.cmd

--- KinashServer_Installer/KinashServer_Folder/KinashServer_Installer/bin/Server-NodeJs.cmd
+++ KinashServer_Installer/KinashServer_Folder/KinashServer_Installer/bin/Server-NodeJs.cmd
@@ -1,6 +1,6 @@
 @echo off
 title Server
-@echo node C:\Users\%username%\Documents\KinashServer\app1.js > %temp%/temp.bat
+@echo node C:\Users\%username%\Documents\KinashServer\server.js > %temp%/temp.bat
 @echo node C:\Users\%username%\Documents\KinashServer\http\443Fix.js > temp2.bat
 start %temp%/temp.bat
 @echo Press ENTER to exit server.
