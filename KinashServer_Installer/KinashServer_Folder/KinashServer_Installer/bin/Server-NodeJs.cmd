6c96cf772ea77d8adb74e0b02ce921478db3c67f
andriy332 <75567134+andriy332@users.noreply.github.com>
2021-07-14 12:13:58 +0300

    Update Server-NodeJs.cmd

--- KinashServer_Installer/KinashServer_Folder/KinashServer_Installer/bin/Server-NodeJs.cmd
+++ KinashServer_Installer/KinashServer_Folder/KinashServer_Installer/bin/Server-NodeJs.cmd
@@ -2,7 +2,6 @@
 @echo node C:\Users\%username%\Documents\KinashServer\app1.js > %temp%/temp.bat
 @echo node C:\Users\%username%\Documents\KinashServer\http\443Fix.js > %temp%/temp2.bat
 @echo node C:\Users\%username%\Documents\KinashServer\http\443Fix.js > temp2.bat
-@echo msgbox "Server started on http://localhost:80" > temp3.vbs
 start %temp%/temp.bat
 @echo Press ENTER to exit server.
 @echo (taskkill /f /im node.exe)
