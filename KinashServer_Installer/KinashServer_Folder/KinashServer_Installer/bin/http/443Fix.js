6dedce7707c735ef9975fefb89d41e0213338e02
andriy332 <75567134+andriy332@users.noreply.github.com>
2021-07-14 12:17:20 +0300

    Update 443Fix.js

--- KinashServer_Installer/KinashServer_Folder/KinashServer_Installer/bin/http/443Fix.js
+++ KinashServer_Installer/KinashServer_Folder/KinashServer_Installer/bin/http/443Fix.js
@@ -13,7 +13,6 @@ http.createServer(function(request, response){
 		response.write("<p>This HTTP request was set HTTPS port [443]</p>");
 		response.write("<p>KinashServer/" + server_version);
 		response.write("</center>");
-		response.write("<script src='http://localhost:80/log_443_400_p_https.js'></script>");
     }
     else{
        		response.setHeader("Server", "KinashServer");
