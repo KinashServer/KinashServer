b8689c77cbb9f7dad9a7ded7ae70ecf13b1ff37e
andriy332 <75567134+andriy332@users.noreply.github.com>
2021-08-31 21:57:51 +0300

    response.setHeader update

--- KinashServer_Installer/bin/server.js
+++ KinashServer_Installer/bin/server.js
@@ -7,6 +7,7 @@ http.createServer(function(request, response){
     console.info('\x1b[32m User: ' + request.method + '  ' + request.url)
     console.info('\x1b[32m Server: 200 OK - ' + 'CustomRequestHeaders: Server: KinashServer')
     response.setHeader("Content-Type", "text/html; charset=utf-8;");
+    response.setHeader("Server", "KinashServer");
     if(request.url === "/"){
            response.setHeader("Server", "KinashServer");
            response.statusCode = 200;
@@ -21,23 +22,19 @@ http.createServer(function(request, response){
  	   console.info('\x1b[33m User: ' + request.method + '  ' + request.url)
   	   console.info('\x1b[33m Server: 302 TempRedirect - ' + 'CustomRequestHeaders: Server: KinashServer')
            response.statusCode = 302;
-           response.setHeader("Server", "KinashServer");
            response.setHeader("Location", "/new");
     }
     else if(request.url == "/new"){
-           response.setHeader("Server", "KinashServer");
            response.write("<p>You are redirected from /old to /new</p>");
     }
     else if(request.url == "/Forbidden"){
  	    console.info('\x1b[31m User: ' + request.method + '  ' + request.url)
   	    console.info('\x1b[31m Server: 403 Forbidden - ' + 'CustomRequestHeaders: Server: KinashServer')
-        response.setHeader("Server", "KinashServer");
        	response.statusCode = 403;
     }
     else{
  	    console.info('\x1b[31m User: ' + request.method + '  ' + request.url)
   	    console.info('\x1b[31m Server: 404NotFound - ' + 'CustomRequestHeaders: Server: KinashServer')
-		response.setHeader("Server", "KinashServer");
     	response.statusCode = 404;
     }
     response.end();
