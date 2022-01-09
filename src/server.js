52c34c82588fcccd17d31b1d5f795c078a60e614
andriy332 <75567134+andriy332@users.noreply.github.com>
2022-01-09 22:08:30 +0200

    Change /. to /..

--- src/server.js
+++ src/server.js
@@ -1,10 +1,9 @@
 const http = require('http')
 const fs = require('fs')
-const folder = './public_html/'
-const config = require('./configs/config.json')
-const log = new console.Console(fs.createWriteStream('./logs/requests-log.txt'))
-const errorlog = new console.Console(fs.createWriteStream('./logs/error-log.txt'))
-const nosslsupport = require('./src/nosslsupport.js')
+const folder = '../public_html/'
+const config = require('../configs/config.json')
+const log = new console.Console(fs.createWriteStream('../logs/requests-log.txt'))
+const errorlog = new console.Console(fs.createWriteStream('../logs/error-log.txt'))
 const mime = require('mime')
 if (!fs.existsSync(folder)) {
   fs.mkdirSync(folder, { recursive: true })
