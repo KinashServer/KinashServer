9560f4212fafc6350decf7b7883b19229b818275
andriy332 <75567134+andriy332@users.noreply.github.com>
2022-01-09 21:28:54 +0200

    Code style fix

--- src/nosslsupport.js
+++ src/nosslsupport.js
@@ -1,19 +1,18 @@
-const http = require('http');
-const config = require('../configs/config.json');
-var page = `<html><head>
+const http = require('http')
+const config = require('../configs/config.json')
+const page = `<html><head>
     <title>Error 500 - No SSL Support</title>
 </head><body>
     <p>SSL not supported</p>
 </body></html>`
 
-
 const requestListener = function (req, res) {
-  res.writeHead(500);
-  res.end(page);
+  res.writeHead(500)
+  res.end(page)
 }
 
-const server = http.createServer(requestListener);
+const server = http.createServer(requestListener)
 
 server.listen(443, config.host, () => {
-  console.log(`\x1b[0m\x1b[32m[INFO] Http error listener started at http://` + config.host + ":" + '443' + "/" );
+  console.log('\x1b[0m\x1b[32m[INFO] Http error listener started at http://' + config.host + ':' + '443' + '/')
 })
