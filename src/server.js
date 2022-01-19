32097848da6412cdd500766368f71b8437cfd7c3
andriy332 <75567134+andriy332@users.noreply.github.com>
2022-01-19 09:55:22 +0200

    I think we need to remove errorlogs

--- src/server.js
+++ src/server.js
@@ -5,7 +5,6 @@ const folder = '../public_html/'
 const config = require('../configs/config.json')
 const log = new console.Console(fs.createWriteStream('./logs/requests-log.txt'))
 const errorlog = new console.Console(fs.createWriteStream('./logs/errors-log.txt'))
-const errorslog = new console.Console(fs.createWriteStream('./logs/errors-log.txt'))
 const mime = require('mime')
 if (!fs.existsSync(folder)) {
   fs.mkdirSync(folder, { recursive: true })
