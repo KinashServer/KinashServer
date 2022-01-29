d8df3086507a1cde8e37da59299e1a3178960c2f
andriy332 <75567134+andriy332@users.noreply.github.com>
2022-01-29 12:51:29 +0200

    Update debug.cmd

--- debug.cmd
+++ debug.cmd
@@ -1,8 +1,12 @@
 @echo off
 
 :startserver
-node server.js --trace-warnings
+
+node app.js --trace-warnings
 @echo ERROR: The server was crashed or stopped
+pause
+cls
+
 goto startserver
 pause
 exit
