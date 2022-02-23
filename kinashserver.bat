5a24f8f5991997b263ce2eb21f0b521fdce1ea44
andriy332 <75567134+andriy332@users.noreply.github.com>
2022-02-23 21:46:49 +0200

    Create kinashserver.bat

---
+++ kinashserver.bat
@@ -0,0 +1,5 @@
+@echo off
+@echo Launching KinashServer on http://localhost:80/
+@echo To get access to server files, and the public_html folder
+@echo Follow this path: C:\users\%username%\node_modules\KinashServer\
+node app.js
