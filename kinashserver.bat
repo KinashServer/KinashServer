c6658b812ce08f1e2cc4274aefdfa200f09f196f
andriy332 <75567134+andriy332@users.noreply.github.com>
2022-03-10 17:15:05 +0200

    [Change log skip] Update kinashserver.bat

--- kinashserver.bat
+++ kinashserver.bat
@@ -1,6 +1,6 @@
 @echo off
 color 3
-@echo  Debug - Launching KinashServer on http://localhost:80/
+@echo  Debug - Launching KinashServer
 @echo  Debug - To get access to server files, and the public_html folder
 @echo  Debug - Follow this path: C:\Users\%username%\node_modules\KinashServer\
 node app.js
