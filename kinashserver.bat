60f06a419441c0265cc41ab406391b66b16c8ceb
andriy332 <75567134+andriy332@users.noreply.github.com>
2022-03-10 17:11:50 +0200

    Fix kinashserver.bat

--- kinashserver.bat
+++ kinashserver.bat
@@ -2,5 +2,5 @@
 color 3
 @echo  Debug - Launching KinashServer on http://localhost:80/
 @echo  Debug - To get access to server files, and the public_html folder
-@echo  Debug - Follow this path: C:\users\%username%\node_modules\KinashServer\
+@echo  Debug - Follow this path: C:\Users\%username%\node_modules\KinashServer\
 node app.js
