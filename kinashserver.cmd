ee23d110ea3722b8e6cbf46e63eddfd38451d491
andriycraft <92477814+andriycraft@users.noreply.github.com>
2022-03-13 14:47:52 +0200

    Remove useless spaces

--- kinashserver.cmd
+++ kinashserver.cmd
@@ -1,6 +1,6 @@
 @echo off
 color 3
-@echo  Debug - Launching KinashServer
-@echo  Debug - To get access to server files, and the public_html folder
-@echo  Debug - Go to this path: C:\Users\%username%\node_modules\KinashServer\
+@echo Debug - Launching KinashServer
+@echo Debug - To get access to server files, and the public_html folder
+@echo Debug - Go to this path: C:\Users\%username%\node_modules\KinashServer\
 node app.js
